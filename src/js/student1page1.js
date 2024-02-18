6f3aff67461a3bbbd6c1e8399a21784cf47bebc9
RobVerbeek <r0932067@student.thomasmore.be>
2024-02-18 20:29:10 +0100

    added projects page

--- src/js/student1page1.js
+++ src/js/student1page1.js
@@ -1,1 +1,7 @@
 // Author:
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus();
+});
