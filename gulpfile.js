0c57f7153b4bfdd3847061eb55a8071ff0a09cf7
RobVerbeek <r0932067@student.thomasmore.be>
2024-02-27 16:42:55 +0100

    complete rebrand of portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('sass', () =>
 // Live-reload the browser
 gulp.task('browser-sync', () => {
     browserSync.init({
-        startPath: '/index.html',
+        startPath: '/index(old).html',
         port: 7777,
         server: {
             baseDir: './src',
