b3a93321415a0339b57ded4c80cff525664276bc
RobVerbeek <r0932067@student.thomasmore.be>
2024-05-08 18:25:24 +0200

    added a blank page, layout fixes, index fixes, javascripting

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-
+window.addEventListener("scroll", function() {
+    var scrollIndicator = document.getElementById("scrollIndicator");
+    if (scrollIndicator) {
+        scrollIndicator.style.display = "none";
+    }
+});
+window.addEventListener("scroll", function() {
+    document.body.classList.add("scrolled");
+});
